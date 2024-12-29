4f1e85cc7e2a900a85625be1bcf16a33e5eccf76
rishikpulipaka <rpuli007@ucr.edu>
2024-12-28 19:22:26 -0800

    - Added socials to header - updated socials to white color - changed some spacing in footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ import mail_icon from "../assets/mail_icon.png"
 
 const Footer = () => {
   return (
-    <div id="footer-container" className="mt-[100px] p-[5px] w-full h-[300px] bg-[#3B3B3B] text-white flex flex-col justify-evenly items-center inset-x-0 bottom-0 font-montserrat">
+    <div id="footer-container" className="mt-[100px] p-[5px] w-full h-[200px] bg-[#3B3B3B] text-white flex flex-col justify-evenly items-center inset-x-0 bottom-0 font-montserrat">
         <div id="socials-container" className="flex border-0 border-red-300 w-[250px] h-[50px] text-white justify-evenly items-center p-2">
           <a href="https://www.instagram.com/">
             <div className="size-[30px] border-0 border-red-500">
@@ -22,11 +22,11 @@ const Footer = () => {
             </div>
           </a>
         </div>
-        <div id="copyright-container" className="">
-            <div className="self-center mt-12 text-base text-center text-white tracking-[2.85px] max-md:mt-10">
-                © 2024 Rishik Pulipaka. All Rights Reserved.
-            </div>
+        
+        <div id="copyright-container" className="self-center mt-12 text-base text-center text-white tracking-[2.85px] max-md:mt-10">
+            © 2024 Rishik Pulipaka. All Rights Reserved.
         </div>
+        
     </div>
   )
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,3 +1,7 @@
+import insta_icon from "../assets/instagram_icon.png"
+import pinterest_icon from "../assets/pinterest_icon.png"
+import mail_icon from "../assets/mail_icon.png"
+
 const Header = () => {
   return (
     <header id="navbar" className="bg-[#76726A] px-[120px] w-100 h-[151px] flex items-center justify-between text-white font-montserrat">
@@ -25,6 +29,9 @@ const Header = () => {
             <li className="hover:scale-125 ease-in-out duration-200"><div className="p-2"><a href="/about">about</a></div></li>
             <li className="hover:scale-125 ease-in-out duration-200"><div className="p-2"><a href="/gallery">gallery</a></div></li>
             <li className="hover:scale-125 ease-in-out duration-200"><div className="p-2"><a href="/booking">booking</a></div></li>
+            <li className="hover:scale-125 ease-in-out duration-200"><div className="p-2"><a href="https://www.instagram.com/"><img src={insta_icon} alt="instagram social icon" className="size-[25px]" /></a></div></li>
+            <li className="hover:scale-125 ease-in-out duration-200"><div className="p-2"><a href="https://www.pinterest.com/"><img src={pinterest_icon} alt="pinterest social icon" className="size-[25px]" /></a></div></li>
+            <li className="hover:scale-125 ease-in-out duration-200"><div className="p-2"><a href="https://www.google.com/"><img src={mail_icon} alt="mail social icon" className="size-[25px]" /></a></div></li>
           </ul>
         </nav>
 
