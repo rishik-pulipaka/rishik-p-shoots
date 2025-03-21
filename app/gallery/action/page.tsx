110b9f7fb978699be7258facfd7768a1ae74c1f5
rishikpulipaka <rpuli007@ucr.edu>
2025-03-21 00:23:55 -0700

    added masonry to action page

--- app/gallery/action/page.tsx
+++ app/gallery/action/page.tsx
@@ -1,27 +1,34 @@
 import Title from "@/app/components/Title";
 import Image from "next/image";
 import { actionItems } from "@/app/data/action";
+import Masonry from "react-masonry-css";
 
 export default function ActionGallery() {
   return (
     <div>
       <Title text="action"/>
       <div id="photos-container" className="p-5">
-        <ul className="flex flex-wrap gap-[7px]">
-            {actionItems.map((item, i) => (
-              <li key={i} className="flex-grow h-[40vh]">
-                <Image
-                  src={item.image}
-                  alt={item.title}
-                  layout="responsive"
-                  loading="lazy"
-                  className="max-h-full min-w-full object-cover align-bottom"
-                />
-              </li>
-            ))}
-
-            <li className="flex-grow-[1000] h-[40vh]"></li>
-        </ul>
+        <Masonry
+          breakpointCols={{
+            default: 4,
+            1100: 3,
+            768: 2,
+            500: 1
+          }} 
+          className="flex gap-2"
+          columnClassName=""
+        >
+          {actionItems.map((item, i) => (
+            <Image 
+              key={i}
+              src={item.image}
+              alt={item.title}
+              className="my-2"
+              layout="responsive"
+              loading="lazy"
+            />
+          ))}
+        </Masonry>
       </div>
     </div>
   );
