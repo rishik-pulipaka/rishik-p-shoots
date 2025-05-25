890b45771a0e788f2ed6a9cf3611ed603402a483
rishikpulipaka <rpuli007@ucr.edu>
2025-05-24 18:53:41 -0700

    added analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,10 +2,13 @@ import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import "./globals.css";
 
+import { Analytics } from "@vercel/analytics/next"
+
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
+      <Analytics />
       <body className="bg-primary flex flex-col min-h-svh">
         <Navbar />
         <main className="flex-grow">{children}</main>
