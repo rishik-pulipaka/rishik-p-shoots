f628ebf6bf3a85e6f840082dd9b1c2f5ac3427bc
rishikpulipaka <rpuli007@ucr.edu>
2025-05-24 19:02:19 -0700

    fix analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,11 +8,11 @@ import { Analytics } from "@vercel/analytics/next"
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <Analytics />
       <body className="bg-primary flex flex-col min-h-svh">
         <Navbar />
         <main className="flex-grow">{children}</main>
         <Footer />
+        <Analytics />
       </body>
     </html>
   );
